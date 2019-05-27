183190abaad0d69dac5086e6467a5fc6b0f23d25
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-05-26 21:21:48 -0500

    test

--- addressBook.js
+++ addressBook.js
@@ -1,73 +1,79 @@
-const newArray = [];
+//'use strict';
 
-function get(){
-    // Fetch a new user multiple times and store them in an array.
-    fetch('https://randomuser.me/api/')
-        .then(res => res.json())
-        .then(data => {
-            newArray.push(data.results[0])
-            console.log(newArray);
-        })
-        // Used this to stop the array from repeating itself on the page
-        document.getElementById("contacts").innerHTML = " ";
+const key = 'abc123';
 
-        // Then list out all the user in your address book array by name and picture.
-        newArray.map(person => {
-            console.log(person);
-            let createLi = document.createElement("li");
-            let contactList = document.getElementById("contacts");
-            let image = document.createElement("img");
-            let button = document.createElement('button');
-            button.addEventListener("click",(e) => {
-                let textBox = document.createElement('p');
-                let pText = document.createTextNode("Cell: " + person.cell + " " + "Age: " + person.dob.age);
-                textBox.appendChild(pText);
-                createLi.appendChild(textBox);
-            })
-            image.src = person.picture.thumbnail;
-            createLi.appendChild(image);
-            createLi.appendChild(document.createTextNode(person.name.first + " " + person.name.last));
-            createLi.appendChild(button);
-            let buttonText = document.createTextNode("More Info");
-            button.appendChild(buttonText);
-            contactList.append(createLi);
-        })
-}
-get();
+const assert = require('assert');
+
+// const newArray = [];
+
+// function get(){
+//     // Fetch a new user multiple times and store them in an array.
+//     fetch('https://randomuser.me/api/')
+//         .then(res => res.json())
+//         .then(data => {
+//             newArray.push(data.results[0])
+//             console.log(newArray);
+//         })
+//         // Used this to stop the array from repeating itself on the page
+//         document.getElementById("contacts").innerHTML = " ";
+
+//         // Then list out all the user in your address book array by name and picture.
+//         newArray.map(person => {
+//             console.log(person);
+//             let createLi = document.createElement("li");
+//             let contactList = document.getElementById("contacts");
+//             let image = document.createElement("img");
+//             let button = document.createElement('button');
+//             button.addEventListener("click",(e) => {
+//                 let textBox = document.createElement('p');
+//                 let pText = document.createTextNode("Cell: " + person.cell + " " + "Age: " + person.dob.age);
+//                 textBox.appendChild(pText);
+//                 createLi.appendChild(textBox);
+//             })
+//             image.src = person.picture.thumbnail;
+//             createLi.appendChild(image);
+//             createLi.appendChild(document.createTextNode(person.name.first + " " + person.name.last));
+//             createLi.appendChild(button);
+//             let buttonText = document.createTextNode("More Info");
+//             button.appendChild(buttonText);
+//             contactList.append(createLi);
+//         })
+// }
+// get();
 
 let multipleArray = [];
 
-function allUsers() {
-    fetch('https://randomuser.me/api/?results=15')
+function allUsers(fetch, number) {
+    fetch('https://randomuser.me/api/?results=' + number)
         .then (res => res.json())
         .then(data => {
             multipleArray.push(data.results)
             console.log(multipleArray);
         })
         // Used this to stop the array from repeating itself on the page
-        document.getElementById("allContacts").innerHTML = " ";
+        //document.getElementById("allContacts").innerHTML = " ";
         
         // Then list out all the user in your address book array by name and picture.
-        multipleArray[0].map(person => {
-            console.log(person);
-            let createLi = document.createElement("li");
-            let contactList = document.getElementById("allContacts");
-            let image = document.createElement("img");
-            let button = document.createElement('button');
-            button.addEventListener("click",(e) => {
-                let textBox = document.createElement('p');
-                let pText = document.createTextNode("Cell: " + person.cell + " " + "Age: " + person.dob.age);
-                textBox.appendChild(pText);
-                createLi.appendChild(textBox);
-            })
-            image.src = person.picture.thumbnail;
-            createLi.appendChild(image);
-            createLi.appendChild(document.createTextNode(person.name.first + " " + person.name.last));
-            createLi.appendChild(button);
-            let buttonText = document.createTextNode("More Info");
-            button.appendChild(buttonText);
-            contactList.append(createLi);
-        });
+        // multipleArray[0].map(person => {
+        //     console.log(person);
+        //     let createLi = document.createElement("li");
+        //     let contactList = document.getElementById("allContacts");
+        //     let image = document.createElement("img");
+        //     let button = document.createElement('button');
+        //     button.addEventListener("click",(e) => {
+        //         let textBox = document.createElement('p');
+        //         let pText = document.createTextNode("Cell: " + person.cell + " " + "Age: " + person.dob.age);
+        //         textBox.appendChild(pText);
+        //         createLi.appendChild(textBox);
+        //     })
+        //     image.src = person.picture.thumbnail;
+        //     createLi.appendChild(image);
+        //     createLi.appendChild(document.createTextNode(person.name.first + " " + person.name.last));
+        //     createLi.appendChild(button);
+        //     let buttonText = document.createTextNode("More Info");
+        //     button.appendChild(buttonText);
+        //     contactList.append(createLi);
+        // });
     multipleArray = [];
 }
 
@@ -76,6 +82,27 @@ setTimeout(function(){allUsers();}, 500);
 }
 delayFunction();
 
-window.onload = function() {
-    allUsers()
-}
+// window.onload = function() {
+//     allUsers()
+// }
+
+describe('allUsers', () => {
+    it ('tests fetch and numbers', () => {
+        const testFetch = url => {
+            assert (
+                url === 'https://randomuser.me/api/?results=15'
+            )
+            return new Promise(function(){})
+        }
+        allUsers(testFetch, 15)
+    })
+    it ('tests fetch and numbers', () => {
+        const testFetch = url => {
+            assert (
+                url === 'https://randomuser.me/api/?results=5001'
+            )
+            return new Promise(function(){})
+        }
+        allUsers(testFetch, 5001)
+    })
+})
